60f862851759d572e8c32aa4ee8e4824670ec22b
tutt1999nd1 <43142674+tutt1999nd1@users.noreply.github.com>
2020-12-15 01:30:11 +0700

    thêm thống kê

--- repos/nhietdo.js
+++ repos/nhietdo.js
@@ -3,6 +3,16 @@ exports.loadAll = () => {
     var sql = 'select * from nhiet_do';
     return db.load(sql);
 }
+exports.loadNhietDoTrongNgay = (date) => {
+    var sql = `select * from nhiet_do where thoi_gian like '${date}'`;
+
+    return db.load(sql);
+}
+exports.loadNhietDoTrongTuan = (date) => {
+    var sql = `select * from nhiet_do where thoi_gian like '${date}'`;
+
+    return db.load(sql);
+}
 exports.add = (data) => {
     var sql = `insert into nhiet_do(nhiet_do,thoi_gian) values ('${data.nhiet_do}','${data.thoi_gian}')`;
     return db.load(sql);
--- routes/index.js
+++ routes/index.js
@@ -30,13 +30,16 @@ wss.on('connection', (ws, req) => {
     console.log(msg);
     // index++;
     ws.send(msg);
-    console.log('t',moment().format('MMMM Do YYYY, h:mm:ss a'));
+    console.log('t',moment().format('MMMM Do YYYY,h:mm:ss '));
     var data={
       nhiet_do:msg,
-      thoi_gian:moment().format('MMMM Do YYYY, h:mm:ss a')
+      thoi_gian:moment().format('HH:mm:ss DD-MM-YYYY ')
     }
-    console.log('hour',moment().seconds())
-    if(moment().hours()==0&&moment().seconds()==0) nhet_do_rep.add(data);
+    if(moment().hours()==0&&moment().seconds()==0&&moment().minutes()==0)
+      nhet_do_rep.add(data);
+    // if(i<13)      nhet_do_rep.add(data);
+
+    // console.log(moment().minutes())
   })
 })
 
@@ -49,6 +52,29 @@ router.get('/', function(req, res, next) {
   //
   // })
   res.render('index', { title: 'Express' });
+});
+router.get('/nhietdo', function(req, res, next) {
+  // nhet_do_rep.loadAll().then(function (data) {
+  //   console.log(data);
+  //   res.render('index', { title: 'Express' });
+  //
+  // })
+  // res.render('index', { title: 'Express' });
+  nhet_do_rep.loadNhietDoTrongNgay('%'+moment().format('DD-MM-YYYY')+'%').then(function (data) {
+    console.log('kaka')
+    res.json(data);
+  })
+});
+router.get('/nhietdoAll', function(req, res, next) {
+  // nhet_do_rep.loadAll().then(function (data) {
+  //   console.log(data);
+  //   res.render('index', { title: 'Express' });
+  //
+  // })
+  // res.render('index', { title: 'Express' });
+  nhet_do_rep.loadAll().then(function (data) {
+    res.json(data);
+  })
 });
 
 module.exports = router;
