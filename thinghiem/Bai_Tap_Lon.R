711bd33e2b1dce21e581f5628c303067e3486ef3
tutt1999nd1 <43142674+tutt1999nd1@users.noreply.github.com>
2020-12-18 23:44:10 +0700

    source code

--- thinghiem/Bai_Tap_Lon.R
+++ thinghiem/Bai_Tap_Lon.R
@@ -6,14 +6,18 @@ expand.grid(d=c(0.5, 1.0),t1=-275, tdoduoc=-275)
 library(daewr)
 Fpower1(alpha = 0.05, nlev=2,nreps=3:10,Delta = 0.05,sigma=0.03)
 # alpha = 0.05,beta= 1-power= 0.128
-# so lan lap = 6
+# so lan lap = 7
 plan <- expand.grid(d=c(0.5, 1.0),t1=-275, tdoduoc=-275)
 plan
 class(plan)
 library(mefa)
-rep(plan,6)
-exp
-exp <- rep(plan,6)
+rep(plan,7)
+exp <- rep(plan,7)
 exp <- exp[sample(1:nrow(exp)),]
-exp
+write.csv(exp,'exp.csv',row.names = FALSE)
+kq <- read.csv('exp.csv')
+kq
+model <- lm(data=kq, d ~ tdoduoc +t1)
+model
+
 
